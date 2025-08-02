4180234c8d05ceea50d100db365b08dbb553efa1
Sara Affara <affarasara@gmail.com>
2025-08-02 10:46:14 -0600

    Spawn Point Update
    - created scripts for the spawn_point scene
    - implemented spawn point budget
    - implemented enemy weight's and cost
    - enemies spawn in (relatively) random intervals

--- wave_counter.gd
+++ wave_counter.gd
@@ -2,9 +2,19 @@ extends Area2D
 
 var counter := 0
 
+# Called when player enters wave counter
 func _on_body_entered(body: Node2D) -> void:
-	# when player passes wave counter, increment counter by 1, and increase strength of current
 	if body.is_in_group("player"):
-		counter+=1
-		print("Counter: ", counter)
+		counter += 1
+		print("Wave Counter: ", counter)
+
+		# notify river current system (if applicable)
 		get_node("/root/World/RiverCurrents").on_player_pass_wave_counter()
+
+		# Update all enemy spawn points with this new wave number
+		for spawn_point in get_tree().get_nodes_in_group("spawn_point"):
+			spawn_point.set_budget_for_wave(counter)
+
+# Returns the current wave number
+func get_current_wave() -> int:
+	return counter
