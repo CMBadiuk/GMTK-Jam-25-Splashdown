24e92fe7690bc6c5ba5f7147efe9e99c4fb2a6d8
CMBadiuk <cmbadiuk@gmail.com>
2025-08-02 22:22:26 -0600

    Menu and Art Work
    - Added more details to main menu
    - Created first executable copy of the game (not included in Github due to file space restrictions

--- main_menu.gd
+++ main_menu.gd
@@ -1,15 +1,22 @@
 extends Control
 
 @onready var controls_screen = $ControlsScreen
+@onready var credits_screen = $CreditsScreen
 
 func _on_start_button_pressed() -> void:
 	SceneManager.goto_scene("res://world.tscn")
 
 func _on_controls_button_pressed() -> void:
 	controls_screen.visible = true
+	
+func _on_credits_button_pressed() -> void:
+	credits_screen.visible = true
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 func _on_back_button_pressed() -> void:
 	controls_screen.visible = false
+	
+func _on_credback_button_pressed() -> void:
+	credits_screen.visible = false
